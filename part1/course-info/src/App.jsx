2bbd31b6024680a9f3c3a4360f323519c4306206
awellsbiz <awellsbiz@gmail.com>
2023-10-19 15:20:04 -0700

    prop passing w react

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,3 +1,36 @@
+function Header(props) {
+  return(
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+function Content(props) {
+ 
+  return(
+    <div>
+      <p>
+      {props.part1} {props.excercises1}
+     </p>
+     <p>
+      {props.part2} {props.excercises2}
+     </p>
+     <p>
+      {props.part3} {props.excercises3}
+     </p>
+    </div>
+  )
+}
+
+function Total(props) {
+  return(
+    <div>
+       <p>Number of excercises {props.excercises1 + props.excercises2 + props.excercises3}</p>
+    </div>
+  )
+}
+
 function App() {
   const course = 'Half stack app development'
   const part1 = 'Fundementals of react'
@@ -8,19 +41,27 @@ function App() {
   const excercises3 = 14
 
   return (
-    <>
-     <h1>{course}</h1>
-     <p>
-      {part1} {excercises1}
-     </p>
-     <p>
-      {part2} {excercises2}
-     </p>
-     <p>
-      {part3} {excercises3}
-     </p>
-     <p>Number of excercises {excercises1 + excercises2 + excercises3}</p>
-    </>
+    <div>
+    <Header
+    course = {course}
+    />
+
+    <Content
+    part1 = {part1}
+    excercises1 = {excercises1}
+    part2 = {part2}
+    excercises2 = {excercises2}
+    part3 = {part3}
+    excercises3 = {excercises3}
+    />
+
+    <Total 
+    excercises1 = {excercises1}
+    excercises2 = {excercises2}
+    excercises3 = {excercises3}
+    />
+    
+    </div>
   )
 }
 
