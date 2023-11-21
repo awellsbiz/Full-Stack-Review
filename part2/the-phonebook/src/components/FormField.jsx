321f263c577f51a3cae68a3da6fe93c3ee0a9e11
awellsbiz <awellsbiz@gmail.com>
2023-11-20 20:41:54 -0800

    component refactor

--- part2/the-phonebook/src/components/FormField.jsx
+++ part2/the-phonebook/src/components/FormField.jsx
@@ -1,12 +1,18 @@
-import Button from "./Button"
-function FormField(props){
-    return (
+import Button from "./Button";
+import InputField from "./InputField";
+
+function FormField(props) {
+  return (
     <form onSubmit={props.handleAction}>
-        <label htmlFor={props.inputName}>{props.inputName} </label>
-        <input name={props.inputName} value={props.value} onChange={props.onChange}/>
-        <Button type={props.btnType} text={props.btnText} />
+      <label htmlFor={props.inputName}>{props.inputName} </label>
+      <InputField
+        name={props.inputName}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      <Button type={props.btnType} text={props.btnText} />
     </form>
-    )
+  );
 }
 
-export default FormField
+export default FormField;
---
+++ part2/the-phonebook/src/components/InputField.jsx
@@ -0,0 +1,5 @@
+function InputField({name, value, onChange}){
+    return <input name={name} value={value} onChange={onChange} />
+}
+
+export default InputField
